cbaf8c09e39c06722b02143d38a2a4f149c92aa9
Jones <36283056+Solositeous@users.noreply.github.com>
2021-12-29 23:28:16 +1000

    updates

--- cl_ui.lua
+++ cl_ui.lua
@@ -4,7 +4,7 @@ exports("uiCreateCustom", function(identifier, addon, htmladd)
 	if uiCreateCustom[identifier] then print("uiCreateCustom Identifier already in use") return end
 	uiCreateCustom[identifier] = true
 	Citizen.CreateThread(function()
-		Wait(5000)
+		Wait(2000)
 		SendNUIMessage({
 			addon = "ui",
 			table = {
@@ -26,9 +26,9 @@ end)
 local uiRegisterCallback = {}
 
 exports("uiRegisterCallback", function(identifier, name, func)
-	if uiRegisterCallback[identifier] then print("uiRegisterCallback Identifier already in use") return end
+	if uiRegisterCallback[identifier..":"..name] then print("uiRegisterCallback Identifier already in use") return end
 	RegisterNUICallback(identifier..":"..name, func)
-	uiRegisterCallback[identifier] = true
+	uiRegisterCallback[identifier..":"..name] = true
 end)
 
 RegisterNUICallback("post", function(data)
@@ -38,6 +38,51 @@ RegisterNUICallback("post", function(data)
 	})
 end)
 
-exports("uiSetFocus", function(keyboard, mouse)
+exports("uiSetFocus", function(identifier, keyboard, mouse)
 	SetNuiFocus(keyboard, mouse)
+	SendNUIMessage({
+		addon = "ui-focus",
+		table = {
+			identifier = identifier,
+			focus = mouse
+		},
+	})
+end)
+
+exports("uiDisableAll", function()
+	SendNUIMessage({
+		addon = "controls",
+		table = {
+			control = "hideAll",
+		}
+	})
+end)
+
+exports("uiEnableAll", function()
+	SendNUIMessage({
+		addon = "controls",
+		table = {
+			control = "showAll",
+		}
+	})
+end)
+
+exports("uiEnable", function(identifier)
+	SendNUIMessage({
+		addon = "controls",
+		table = {
+			identifier = identifier,
+			control = "show",
+		}
+	})
+end)
+
+exports("uiDisable", function(identifier)
+	SendNUIMessage({
+		addon = "controls",
+		table = {
+			identifier = identifier,
+			control = "hide",
+		}
+	})
 end)
--- f_script.js
+++ f_script.js
@@ -1,8 +1,32 @@
 $(function () {
 	var uiList = [];
-	window.addEventListener('message', event => {
-		var data = event.data
-		var skip = false
+	
+	function updateScale() {
+		var scalex = ($("#ui-html").width() / 1920);
+		var scaley = ($("#ui-html").height() / 1080);
+		$(".ui-body").css({"transform-origin-x": 0, "transform-origin-y": 0});
+		if (scalex > scaley) {
+			scalex = scaley
+			var x = ($("#ui-html").width() / scalex) - 1920;
+			var y = ($("#ui-html").height() / scaley) - 1080;
+			$(".ui-body").css({"transform-origin-x": x, "transform-origin-y": y});
+		} else if (scaley > scalex) {
+			scaley = scalex
+			var x = ($("#ui-html").width() / scalex) - 1920;
+			var y = ($("#ui-html").height() / scaley) - 1080;
+			$(".ui-body").css({"transform-origin-x": x, "transform-origin-y": y});
+		}
+		$(".ui-body").css("transform", "scale("+scalex+", "+scaley+")")
+		.width("1920px")
+		.height("1080px");
+	}
+
+	updateScale();
+
+	window.addEventListener("message", event => {
+		updateScale();
+		var data = event.data;
+		var skip = false;
 		if (data.addon == "ui") {
 			if (data.table.identifier != null) {
 				uiList.forEach(function(item, index) {
@@ -13,22 +37,49 @@ $(function () {
 				if (skip) return false;
 				$("<iframe>")
 				.attr("src", "nui://"+data.table.addonname+"/"+data.table.htmladd)
-				.attr('name', data.table.addonname+"/"+data.table.identifier)
-				.attr('id', data.table.identifier)
-				.attr('allow', "microphone *; camera *;")
-				.attr('tabindex', "-1")
-				.attr('style', "visibility: visible; z-index: 0;")
-				.appendTo('.ui-body');
+				.attr("name", data.table.addonname+"/"+data.table.identifier)
+				.attr("id", data.table.identifier)
+				.attr("allow", "microphone *; camera *;")
+				.attr("tabindex", "-1")
+				.attr("style", "visibility: visible; z-index: 0;")
+				.appendTo(".ui-body");
 				uiList.push(data.table.identifier);
 			}
 		} else if (data.addon == "ui-post") {
-			$.post('http://ui-wrapper/' + data.table.identifier + ":" + data.table.name, JSON.stringify(data.table.args));
+			$.post("http://SSCore/" + data.table.identifier + ":" + data.table.name, JSON.stringify(data.table.args));
+		} else if (data.addon == "ui-focus") {
+			if (data.table.focus) {
+				$("#" + data.table.identifier).attr("style", "z-index: 999;");
+			} else {
+				$("#" + data.table.identifier).attr("style", "z-index: 0;");
+			}
+		} else if (data.addon == "controls") {
+			if (data.table.control == "hide") {
+				$("#" + data.table.identifier).hide();
+			} else if (data.table.control == "show") {
+				$("#" + data.table.identifier).show();
+			} else if (data.table.control == "showAll") {
+				uiList.forEach(function(item, index) {
+					$("#" + item).show();
+				})
+			} else if (data.table.control == "hideAll") {
+				uiList.forEach(function(item, index) {
+					$("#" + item).hide();
+				})
+			}
 		} else if (data.addon != null) {
-			$(".ui-body").children("iframe").each(function () {
-				if (data.addon == $(this).context.id) {
-					$(this).context.contentWindow.postMessage(data.table, "*");
-				}
-			})
+			try {
+				$("#" + data.addon)[0].contentWindow.postMessage(data.table, "*");
+			} catch(err) {
+			}
 		}
+	});
+
+	window.addEventListener('keydown', function (event) {
+		uiList.forEach(function(item, index) {
+			$("#" + item)[0].contentWindow.dispatchEvent(
+				new KeyboardEvent('keydown', {key: event.key})
+			);
+		});
 	});
 });
